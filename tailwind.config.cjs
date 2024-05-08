9c3ba1514a22d03d9aae0ddaf790c9e74a46f888
wilfred <wilfred.obeng@stu.ucc.edu.gh>
2024-05-08 02:09:09 +0000

    Updates sans font to Lato

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     darkMode: 'class',
     theme: {
         fontFamily: {
-            sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            sans: ['Lato', 'Inter', ...defaultTheme.fontFamily.sans],
             serif: ['Newsreader', ...defaultTheme.fontFamily.serif]
         },
         extend: {
